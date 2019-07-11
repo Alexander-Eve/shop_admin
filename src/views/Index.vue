631c98d203e0da8aad35c3a45c0a0187c46f03c0
Alexander-Eve <840886969@qq.com>
2019-07-11 17:25:43 +0800

    index完成

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,14 +2,54 @@
   <div class="index">
     <el-container>
       <el-header>
-        <el-row :gutter="20">
-          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-          <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
-          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        </el-row>
+        <div class="logo"></div>
+        <div class="title">
+          <h1>电商后台管理系统</h1>
+        </div>
+        <div class="quit">
+          欢迎光临，xxx <a href="javascript:;">退出</a>
+        </div>
       </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+          <el-menu
+            unique-opened
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">用户列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>权限管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-3">用户列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>商品管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-2">用户列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
@@ -18,6 +58,14 @@
 
 <script>
 export default {
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
 }
 </script>
 
@@ -25,75 +73,40 @@ export default {
 .index {
   height: 100%;
 }
-.el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-    .logo {
-      float: left;
-      height: 100%;
-      width: 180px;
-      img {
-        width: 100%;
-        // height: 100%;
-      }
-    }
-    .quit {
-      float: right;
-      height: 100%;
-    }
-    .title {
-      overflow: hidden;
-      height: 100%;
-      font-size: 30px;
-      color: white;
-      font-weight: 700;
-    }
-  }
-
-.el-aside {
-  background-color: #D3DCE6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #E9EEF3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
 .el-container {
-height: 100%;
-}
-.el-row {
-  margin-bottom: 20px;
-  &:last-child {
-    margin-bottom: 0;
-  }
-}
-.el-col {
-  border-radius: 4px;
   height: 100%;
 }
-.bg-purple-dark {
-  background: #99a9bf;
+.el-header {
+  line-height: 60px;
+  display: flex;
+  background: url('../assets/1000113_300.jpg') no-repeat center/cover;
+  .logo,
+  .quit {
+    width: 180px;
+  }
+  .logo {
+    background: url('../assets/logo.png') no-repeat center center/contain ;
+  }
+  .quit {
+    a {
+      color: peru;
+      font-weight: 550;
+    }
+  }
+  .title {
+    flex: 1;
+    h1 {
+      font-size: 30px;
+      color: #596;
+      text-align: center;
+    }
+  }
 }
-.bg-purple {
-  background: #d3dce6;
+.el-aside {
+  background-color: #545c64;
+  .el-submenu {
+    width: 200px;
+  }
 }
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 60px;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
-}
+
 </style>
